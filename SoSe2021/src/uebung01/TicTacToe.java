58ae9403fdb649875d258367c301499af8937b87
alla1707 <a_massold@mail.ru>
2021-05-03 17:44:26 +0200

    init commit

---
+++ SoSe2021/src/uebung01/TicTacToe.java
@@ -0,0 +1,29 @@
+package uebung01;
+
+public class TicTacToe {
+
+	    enum State {EMPTY, RED, BLACK};
+	    State[][] field;
+
+	    public TicTacToe()
+	    {
+	        field = new State[3][3];
+	        for(int i=0; i<field.length; i++)
+	          for(int j=0; j<field[i].length; j++)
+	            field[i][j]=State.EMPTY;
+	    }
+
+	    public void makeMove(int i, int j, State player)
+	    {
+	        if(field[i][j]==State.EMPTY && player!=State.EMPTY)   
+	            field[i][j]=player;
+	    }
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
